0c7aaede6e4dc7c4bfe5b5bf0979aff238e1398e
Maxime BIDEAU <m.bideau@workstation19.nsi.lan>
2024-10-04 13:33:31 +0200

    version 1

--- Tortue.py
+++ Tortue.py
@@ -1,2 +1,21 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.speed(0) # maximum speed
 
-piuhyoiyghiouyg
+
+
+def equilateral(longueur) :
+    for _ in range (3):
+        t.forward(longueur)
+        t.left(120)
+        
+def carré(longueur) :
+    for _ in range (4):
+        t.forward(longueur)
+        t.left(90)
+        
+        
+l = int(input("Quels longueur veut tu ? "))
+carré(l)
+turtle.exitonclick()
